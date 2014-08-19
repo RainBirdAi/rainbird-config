24e4a0adbe4a724cfdfed3fe35cc272269ec8d7b
Dom Davis <dom@domdavis.com>
2014-08-19 14:40:25 +0100

    Add tests for `getOverride`

--- test/test-environment.js
+++ test/test-environment.js
@@ -6,22 +6,22 @@ var env = require('../lib/environment.js');
 
 describe('shellEscape', function() {
     it('should not alter valid variable names', function(done) {
-        var name = "VALID_VARIABLE_NAME";
+        var name = 'VALID_VARIABLE_NAME';
         expect(env.toVariableName(name)).to.equal(name);
         done();
     });
 
     it('should replace invalid characters with underscore', function(done) {
-        var name = "THE*2ND!VARIABLE-NAME";
-        var expected = "THE_2ND_VARIABLE_NAME";
+        var name = 'THE*2ND!VARIABLE-NAME';
+        var expected = 'THE_2ND_VARIABLE_NAME';
         expect(env.toVariableName(name)).to.equal(expected);
         done();
     });
 
     it('should prefix names starting with a number with underscore',
         function(done) {
-            var name = "2TEST";
-            var expected = "_2TEST";
+            var name = '2TEST';
+            var expected = '_2TEST';
             expect(env.toVariableName(name)).to.equal(expected);
             done();
         }
@@ -30,25 +30,41 @@ describe('shellEscape', function() {
 
 describe('toVariableName', function() {
     it('should allow the prefix to be optional', function(done) {
-        var name = "test";
-        var expected = "TEST";
+        var name = 'test';
+        var expected = 'TEST';
         expect(env.toVariableName(name)).to.equal(expected);
         done();
     });
 
     it('should use the prefix when given', function(done) {
-        var name = "test";
-        var prefix = "A";
-        var expected = "A_TEST";
+        var name = 'test';
+        var prefix = 'A';
+        var expected = 'A_TEST';
         expect(env.toVariableName(name, prefix)).to.equal(expected);
         done();
     });
 
     it('should trim excess spaces', function(done) {
-        var name = "   spaces   ";
-        var prefix = "  with ";
-        var expected = "WITH_SPACES";
+        var name = '   spaces   ';
+        var prefix = '  with ';
+        var expected = 'WITH_SPACES';
         expect(env.toVariableName(name, prefix)).to.equal(expected);
         done();
     });
+});
+
+describe('getOverride', function() {
+    it('should return the value of an environment variable', function() {
+        var value = env.getOverride('STRING_ITEM1', '');
+        expect(value).to.equal('string-item1-env-value');
+    });
+    it('should convert numeric values to numbers', function() {
+        expect(env.getOverride('FLOAT_ITEM1', '')).to.equal(1.1);
+    });
+    it('should return the default value if no environment variable is set',
+        function() {
+            var value = "test";
+            expect(env.getOverride('WONT_BE_SET', value)).to.equal(value);
+        }
+    );
 });
