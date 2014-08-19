98c8d5f4fb997822f60433a92c199beb93001d2c
Dom Davis <dom@domdavis.com>
2014-08-19 13:06:04 +0100

    Implement merging base and config file together
    Remove `getConfigElement` as it's unlikely to get used.

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
+var merge = require('merge');
+
 // ## Initial Setup
 //
-// Setup should be performed before the call to `init`.
-//
+// Setup should be performed before the call to `init` otherwise default values
+// will be used for everything. If `init` isn't called then the config object
+// will be empty.
+
+var config = {};
+var baseConfig = {};
+var environmentPrefix = '';
+
 // Optionally set the location of the base configuration file using
 // `setBaseConfig`. The configuration file should be valid JSON.
 // If no base configuration file is specified then the empty configuration `{}`
@@ -9,7 +17,7 @@
 // for configuration values.
 
 function setBaseConfig(path) {
-    console.log('path: %s', path);
+    baseConfig = require(path);
 }
 
 // Optionally set the prefix for environment variables using
@@ -17,7 +25,7 @@ function setBaseConfig(path) {
 // be built with the prefix `PREFIX_`.
 
 function setEnvironmentPrefix(prefix) {
-    console.log('prefix: %s', prefix);
+    environmentPrefix = prefix;
 }
 
 // Initialise the configuration providing the path to the configuration file.
@@ -25,30 +33,21 @@ function setEnvironmentPrefix(prefix) {
 // `undefined`.
 
 function init(path) {
-    console.log('path: %s', path);
+    var userConfig = require(path);
+    config = merge(baseConfig, userConfig);
 }
 
 // Return the entire configuration JSON using `getConfig`. If `init` has not
 // been called then `getConfig` will simply return `{}`
 
 function getConfig() {
-    return undefined;
-}
-
-// Return the value for a given element using `getConfigElement`. A default
-// value can be given which will be returned if the element hasn't been set in
-// the config. By default `undefined` will be returned if no value is set.
-
-function getConfigElement(element, defaultValue) {
-    console.log('element: %s, defaultValue: %s', element, defaultValue);
-    return defaultValue;
+    return config;
 }
 
 module.exports.setBaseConfig = setBaseConfig;
 module.exports.setEnvironmentPrefix = setEnvironmentPrefix;
 module.exports.init = init;
 module.exports.getConfig = getConfig;
-module.exports.getConfigElement = getConfigElement;
 
 // ## License
 //
