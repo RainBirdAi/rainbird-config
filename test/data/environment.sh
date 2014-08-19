1d7d3133e7ff077752c7f572092da699059d6fd3
Dom Davis <dom@domdavis.com>
2014-08-19 09:24:01 +0100

    Add test data for unit tests.

---
+++ test/data/environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export STRING_ITEM1=string-item1-env-value;
+export BOOLEAN_ITEM1=true;
+export INT_ITEM1=11;
+export FLOAT_ITEM1=1.1;
+export NESTED_ITEMS_NESTED_ITEM1=nested-item1-env-value;
+export LIST_ITEMS2="env-list-item1, env-list-item2, env-list-item3";
+export PREFIX_STRING_ITEM1=string-item1-prefix-value;
